816cf606a9e4f581e60a7006b6ec445d1d07f08d
loivvfx21942 <loivvfx21942@funix.edu.vn>
2023-03-31 15:10:25 +0700

    updateCode

--- charter.js
+++ charter.js
@@ -1,5 +1,5 @@
 let charter = document.getElementsByClassName("projectCharter");
 
 window.addEventListener("click", function () {
-  window.location.href = "http://127.0.0.1:5501/index.html";
+  window.location.href = "https://loivvfx21942.github.io/digital-cv-project/";
 });
